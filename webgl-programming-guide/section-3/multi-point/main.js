00dd2c05134d76c97ad29482b02c47674560900d
jynxio <1337816495@qq.com>
2022-10-12 00:12:44 +0800

    update

--- webgl-programming-guide/section-3/multi-point/main.js
+++ webgl-programming-guide/section-3/multi-point/main.js
@@ -26,10 +26,10 @@ function main () {
     /* 创建缓冲区对象 */
     const vertex_buffer = gl.createBuffer();
 
-    /* 将缓冲区对象绑定到目标 */
+    /* 绑定缓冲区对象（将缓冲区对象绑定至顶点着色器的ARRAY_BUFFER中去，这会表示缓冲区对象中存储的是顶点的位置数据） */
     gl.bindBuffer( gl.ARRAY_BUFFER, vertex_buffer );
 
-    /* 向缓冲区对象写入数据 */
+    /* 向缓冲区对象写入数据（向绑定在ARRAY_BUFFER中的缓冲区对象写入数据） */
     const vertex_count = 3;
     const vertices = new Float32Array( [ 0, 0.5, - 0.5, - 0.5, 0.5, - 0.5 ] );
 
@@ -38,7 +38,7 @@ function main () {
     /* 获取a_Position变量的存储地址 */
     const a_position = gl.getAttribLocation( gl.program, "a_Position" );
 
-    /* 准备将缓冲区对象派送给a_Position变量 */
+    /* 将缓冲区对象派送给a_Position变量（将绑定在ARRAY_BUFFER中的缓冲区对象的内存地址分配给a_Position变量） */
     gl.vertexAttribPointer( a_position, 2, gl.FLOAT, false, 0, 0 );
 
     /* 批准将缓冲区对象派送给a_Position变量 */
@@ -49,6 +49,6 @@ function main () {
     gl.clear( gl.COLOR_BUFFER_BIT );
 
     /* 绘制一个点 */
-    gl.drawArrays( gl.POINTS, 0, 3 );
+    gl.drawArrays( gl.POINTS, 0, vertex_count );
 
 }
